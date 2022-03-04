0e2e4bedc1fce33d7f0a7b4abaf3a04723a9829f
MrRamoun <omartarekramoun@gmail.com>
2022-03-04 23:00:03 +0200

    Fetach all rockets data from an api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import styled from 'styled-components';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Rockets from './components/Rockets/Rockets';
+
+const WholePage = styled.div`
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <Header />
+    <WholePage>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+      </Routes>
+    </WholePage>
   );
 }
 
