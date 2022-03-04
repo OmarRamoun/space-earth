102d3ef524ad2f14706e54537b8fc03ea10c73f8
MrRamoun <omartarekramoun@gmail.com>
2022-03-04 23:13:44 +0200

    Add final edits and commits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Rockets from './components/Rockets/Rockets';
+import Profile from './components/Profile/Profile';
+import Missions from './components/Missions/Missions';
 
 const WholePage = styled.div`
   min-height: 100vh;
@@ -13,6 +15,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Rockets />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/missions" element={<Missions />} />
       </Routes>
     </WholePage>
   );
--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -2,19 +2,27 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { getRocketsFromAPI, listRockets } from '../../redux/rockets/rockets';
+import { getMissionsFromAPI, listMissions } from '../../redux/missions/missions';
 import Rocket from '../Rocket/Rocket';
 
 const RocketsContainer = styled.div`
-  padding: 1rem;
+  padding: 0.3rem 2rem;
 `;
 
 const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rocketsReducer);
+  const missions = useSelector((state) => state.missionsReducer);
 
   useEffect(async () => {
-    const APIrockets = await getRocketsFromAPI();
-    dispatch(listRockets(APIrockets));
+    if (rockets.length === 0) {
+      const APIrockets = await getRocketsFromAPI();
+      dispatch(listRockets(APIrockets));
+    }
+    if (missions.length === 0) {
+      const APImissions = await getMissionsFromAPI();
+      dispatch(listMissions(APImissions));
+    }
   }, []);
 
   return (
